ca6f52970160d6dde58a03c950335d9dc298991e
Artur Fiedorowicz <artur.fiedorowicz@gpj.com>
2018-11-19 16:13:16 +0000

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	// $('#submit').on("click", function() {
+
+		// event.preventDefault();
+		
+		// var Surname = $('#surname').val();
+		// Surname = Surname.toLowerCase();
+
+		// if (Surname == "kerwood") {
+	        // window.location.href = 'pass.html';
+	    // }
+	    // else {
+	        // alert("Sorry, you don't appear to be registered for this event.");
+	    // }
+
+	// });
+
+});
